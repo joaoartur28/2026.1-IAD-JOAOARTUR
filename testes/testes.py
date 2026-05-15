3d226d96b0ea07810aadaabe7e01129229311dc0
joao Artur <joaoartur28@gmail.com>
2026-05-15 10:56:19 -0300

    apagado

--- testes/testes.py
+++
@@ -1,5 +0,0 @@
-n = 10
-while True:
-    print(n, end='')
-    n = n- 1
-print('Pronto!')
--- testes/testes1.py
+++
@@ -1,7 +0,0 @@
-máximo = None
-print('Antes:', máximo)
-for itervar in [3, 41, 12, 9, 74, 15]:
-    if máximo is None or itervar > máximo :
-        máximo = itervar
-    print('Laço:', itervar, máximo)
-print('Máximo:', máximo)
